873f6e7e94bbca1d9bf5e03997493d1cf57dea0e
rbuchynska <buchynska.ruslana@gmail.com>
2024-06-25 12:50:30 +0300

    Lesson 6.5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,18 +18,15 @@ public class Main {
         list.remove(1);
         System.out.println("We anticipate the list of [1, 3] after using the remove() method: ");
         list.forEach(System.out::print);
-        list.add(2, 4);
-        System.out.println("\nWe anticipate the list of [1, 3, 4] after using the add(int index, int element) method: ");
-        list.forEach(System.out::print);
-        System.out.println("\nWe anticipate [true] using the contains() method: " + list.contains(4));
-        System.out.println("We anticipate [2] using the indexOf() method: " + list.indexOf(4));
-        System.out.println("We anticipate [false] using isEmpty() method: " + list.isEmpty());
-        list.clear();
-        System.out.println("We anticipate [true] using isEmpty() after clear() method: " + list.isEmpty());
+
         Set<Integer> set = new HashSet<>();
+        System.out.println("\n\nChecking [CustomArrayList]: \nWe anticipate that the size of the set will be [0]: " + set.size());
         set.add(1);
         set.add(2);
         set.add(3);
-        set.size();
+        System.out.println("We anticipate that the size of the set will be[3]: " + set.size());
+        System.out.println("We anticipate [true] using contains(2) method: " + set.contains(2));
+        set.remove(3);
+        System.out.println("We anticipate that the size of the set will be[2]: " + set.size());
     }
 }
