bf2fe339cd5fbf0a57c7465f1ecd10e200ce6bbd
rbuchynska <buchynska.ruslana@gmail.com>
2024-06-25 15:30:29 +0300

    Lesson 6.5. Added functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.list.CustomArrayList;
+import org.example.set.CustomHashSet;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -19,14 +20,30 @@ public class Main {
         System.out.println("We anticipate the list of [1, 3] after using the remove() method: ");
         list.forEach(System.out::print);
 
-        Set<Integer> set = new HashSet<>();
+        CustomHashSet set = new CustomHashSet();
         System.out.println("\n\nChecking [CustomArrayList]: \nWe anticipate that the size of the set will be [0]: " + set.size());
         set.add(1);
         set.add(2);
         set.add(3);
-        System.out.println("We anticipate that the size of the set will be[3]: " + set.size());
+        set.add(4);
+        set.add(5);
+        set.add(6);
+        set.add(7);
+        set.add(8);
+        set.add(9);
+        set.add(10);
+        set.add(11);
+        set.add(12);
+        set.add(13);
+        set.add(14);
+        set.add(15);
+        set.add(16);
+        set.add(17);
+        set.add(18);
+        set.add(19);
+        System.out.println("We anticipate that the size of the set will be [19]: " + set.size());
         System.out.println("We anticipate [true] using contains(2) method: " + set.contains(2));
         set.remove(3);
-        System.out.println("We anticipate that the size of the set will be[2]: " + set.size());
+        System.out.println("We anticipate that the size of the set will be [18]: " + set.size());
     }
 }
